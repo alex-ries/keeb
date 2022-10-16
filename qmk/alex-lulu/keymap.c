27b1752249b336946ee4cf244b2d8129e6c967df
Alex Ries <alex@familie-ries.nl>
2022-10-16 16:53:52 -0400

    Fixed Tap overlap issues

--- qmk/alex-lulu/keymap.c
+++ qmk/alex-lulu/keymap.c
@@ -183,7 +183,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,    KC_LBRC,    KC_7,       KC_8,     KC_9,     KC_RBRC,                    KC_NO,      KC_NO,      KC_NO,      KC_NO,    QK_BOOT,    KC_NO,
   KC_NO,    KC_SCLN,    KC_4,       KC_5,     KC_6,     KC_EQL,                     KC_NO,      KC_LSFT,    KC_LCTL,    KC_LALT,  KC_LGUI,    KC_NO,
   KC_NO,    KC_GRV,     KC_1,       KC_2,     KC_3,     KC_BSLS,  KC_NO,  KC_NO,    KC_NO,      KC_NO,      KC_NO,      KC_NO,    KC_NO,      KC_NO,
-                        KC_NO,      KC_NO,    KC_NO,    KC_NO,                      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
+                        KC_NO,      KC_NO,    KC_NO,    KC_0,                      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
   ),
 /* Symbol Left
  * ,-----------------------------------------.                    ,-----------------------------------------.
