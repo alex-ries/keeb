829ddffcb25fb3f23a30122d3fdc9410b893da68
Alex Ries <alex@familie-ries.nl>
2022-10-16 12:14:35 -0400

    Removed layer OLED switching

--- qmk/alex-lulu/keymap.c
+++ qmk/alex-lulu/keymap.c
@@ -228,7 +228,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                         KC_NO,      KC_NO,    KC_NO,    KC_NO,                      KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
   )
 };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-   return update_tri_layer_state(state, _BASE, _GAME, _NUMP);
-}
